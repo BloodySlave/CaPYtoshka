01bc5b751ba662636d281bbc3f652a2afa9fbe71
BloodySlave <kamolkin2019@yandex.ru>
2021-06-14 02:30:59 +0500

    New simple PyGame(Just for testing commit and etc.)

--- kek.py
+++ kek.py
@@ -1,4 +1,13 @@
-for i in range(10):
-    while i > 4:
-        print("Norm!")
-print('Good!')
+import random
+#Угадайка!
+a = int(input(' Угадайка число!(Введите цифру от 1 до 5) '))
+b = random.randint(1, 5)
+
+if a == b:
+    print('Вы угадали!')
+elif a > b:
+    print('Увы, Ваше число было больше загаданного!')
+elif a < b:
+    print('Увы, Ваше число было меньше загаданного!')
+else:
+    print('Шо?')
